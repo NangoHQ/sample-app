84ca78f38c01d265c5cbde00a825df51f16ecbc8
Taylor <taylor.r.conlin@gmail.com>
2024-04-11 10:31:36 -0400

    merge

--- back-end/src/routes/getContacts.ts
+++ back-end/src/routes/getContacts.ts
@@ -1,28 +1,18 @@
 import type { RouteHandler } from 'fastify';
+import type { Prisma } from '@prisma/client';
 import { db } from '../db.js';
 
-<<<<<<< HEAD
-const validation = z
-  .object({
-    integration: z.enum(['slack']),
-  })
-  .strict();
-=======
 export type GetContacts = {
-  contacts: Contact[];
+  contacts: Prisma.ContactsMaxAggregateOutputType[];
 };
->>>>>>> eb30b75 (merge)
 
-export const getContacts: RouteHandler = async (req, reply) => {
-  const res = validation.safeParse(req.query);
-  if (!res.success) {
-    await reply.status(400).send({ error: true, msg: res.error });
-    return;
-  }
-
+export const getContacts: RouteHandler<{
+  Querystring: { integration: 'slack' };
+  Reply: GetContacts;
+}> = async (req, reply) => {
   // Get the contacts we saved in our own database
   const contacts = await db.contacts.findMany({
-    where: { integrationId: res.data.integration },
+    where: { integrationId: req.query.integration },
     orderBy: { updatedAt: 'desc' },
     take: 100,
   });
